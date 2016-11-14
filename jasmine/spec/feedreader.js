c7f72b1239a06170752586437346e7e10cdb417f
Chelsea <chelsea@Chelseas-MacBook-Pro.local>
2016-11-14 13:50:43 -0500

    Final commit before appointment
    Get everything synced before appointment

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -110,15 +110,21 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection"
-    //describe('New Feed Selection', function() {
-
+    /* TODO: Write a new test suite named "New Feed Selection"         */
+    describe('New Feed Selection', function() {
+        var feedContents;
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+            feedContents = $('.feed').html();
+        });
 
-    //  it('shows new feed results', function() {
-
-    //  });
+        it('shows new feed results', function(done) {
+            loadFeed(0);
+            expect($('.feed').html()).not.toEqual(feedContents);
+            done();
+        });
 }());
